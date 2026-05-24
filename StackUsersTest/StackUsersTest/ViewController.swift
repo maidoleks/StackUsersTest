0bcae5dec7b591403725b574308641cc428dfac1
Oleksii Maidanyk <oleksii.maidanyk@gmail.com>
2026-05-24 21:17:43 +0200

    Initial commit empty project and gitignore

---
+++ StackUsersTest/StackUsersTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackUsersTest
+//
+//  Created by Oleksii Maidanyk on 24/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
